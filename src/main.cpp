0d7d2b1d5dd694bbad424659028227540cb7c4a9
ahthae <122731557+ahthae@users.noreply.github.com>
2025-01-30 16:28:46 -0800

    Use hardware PWM to move servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,82 @@
 #include <Arduino.h>
-#include <Servo.h>
 #include <Adafruit_MCP2515.h>
 #include <Wire.h>
 
+#include "pico/stdlib.h"
+#include <hardware/pwm.h>
+
 #include "ncr_common.h"
 
-#define SERVO_1_PIN 5
-#define SERVO_2_PIN 6
+#define PID_LABEL 1
 
+#define PIN_SERVO_1 A0
+#define PIN_SERVO_2 A2
+
+#define NUM_SERVOS 2
+
+#define TOP 65535
+#define CC_LOW 6553
+#define CC_HIGH 65535u>>1
+#define DIV_INT 9
+#define DIV_FRAC 9
+
+void move_servo(uint servo_pin, uint degree);
 int map_270_servo(int);
+void on_can_received(int packet_size);
 
-Servo servo;
+// Servo servos[NUM_SERVOS];
 
 Adafruit_MCP2515 can(10);
+uint slice;
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial) delay(10);
+  // pinMode(LED_BUILTIN, OUTPUT);
+  // digitalWrite(LED_BUILTIN, LOW);
+  gpio_set_function(PIN_SERVO_1, GPIO_FUNC_PWM);
+  slice = pwm_gpio_to_slice_num(PIN_SERVO_1);
+  pwm_set_gpio_level(PIN_SERVO_1, CC_LOW);
+  pwm_set_clkdiv_int_frac4(slice, DIV_INT, DIV_FRAC);
+  pwm_set_enabled(slice, true);
 
-  if (!can.begin(CAN_BAUD)) {
-   
+  // Serial.begin(9600);
+
+/*   while(!can.begin(CAN_BAUD)) {
+    digitalWrite(LED_BUILTIN, HIGH);
     Serial.println("Error initializing MCP2515.");
-    while(1) delay(10);
+    delay(100);
   }
-  Serial.println("MCP2515 found."); 
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("MCP2515 found.");  */
 
-  servo.attach(SERVO_1_PIN, 500, 2500);
+/*   servos[0].attach(PIN_SERVO_1, 500, 2500);
+  servos[1].attach(PIN_SERVO_2, 500, 2500); */
+
+  // can.onReceive(PIN_CAN_INTERRUPT, on_can_received);
 }
 
 void loop() {
-  servo.write(120);
-  delay(5000);
-  servo.write(map_270_servo(0));
-  delay(5000);
+  move_servo(PIN_SERVO_1, 135);
+  delay(2000);
+  move_servo(PIN_SERVO_1, 225);
+  delay(2000);
+          
+  // servos[0].write(120);
+  // delay(5000);
+  // digitalWrite(LED_BUILTIN, HIGH);
+  // servos[0].write(map_270_servo(0));
+  // delay(5000);
+  // digitalWrite(LED_BUILTIN, LOW);
+}
+
+void move_servo(uint servo_pin, uint degree) {
+  uint16_t cc = map(degree, 0, 270, CC_LOW, CC_HIGH);
+  pwm_set_gpio_level(servo_pin, cc);
 }
 
 int map_270_servo(int angle) {
   return map(angle, 0, 270, 0, 180);
+}
+
+void on_can_received(int packet_size) {
+  // TODO parse & execute valve command
 }
