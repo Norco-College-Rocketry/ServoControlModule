f36b9293e394c04102affecf5567cd797ca3dd13
ahthae <122731557+ahthae@users.noreply.github.com>
2025-02-06 22:17:38 -0800

    Implment valve command

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,12 @@
 
 #include "ncr_common.h"
 
-#define PID_LABEL 1
+#define CAN_ID 0x402
+#define SERVO_1_ID 0x60
+#define SERVO_2_ID 0x61
+
+#define DEG_CLOSED 140
+#define DEG_OPEN 230
 
 #define PIN_SERVO_1 A0
 #define PIN_SERVO_2 A2
@@ -30,6 +35,7 @@ uint servos[NUM_SERVOS] = { PIN_SERVO_1, PIN_SERVO_2 };
 Adafruit_MCP2515 can(PIN_CAN_CS);
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 
   Serial.begin(9600);
@@ -43,25 +49,22 @@ void setup() {
   can.onReceive(PIN_CAN_INTERRUPT, on_can_received);
 
   // Configure GPIO and PWM slices
-  pinMode(LED_BUILTIN, OUTPUT);
   for (size_t i=0; i<NUM_SERVOS; i++) {
     gpio_set_function(servos[i], GPIO_FUNC_PWM);
-    pwm_set_gpio_level(servos[i], CC_LOW);
 
     uint slice = pwm_gpio_to_slice_num(servos[i]);
     pwm_set_clkdiv_int_frac4(slice, DIV_INT, DIV_FRAC);
     pwm_set_enabled(slice, true);
   }
+
+  for (size_t i=0; i<NUM_SERVOS; i++) {
+    move_servo(servos[i], DEG_CLOSED);
+  }
   
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void loop() {
-  for (size_t i=0; i<NUM_SERVOS; i++) { move_servo(servos[i], 135); }
-  delay(2000);
-  for (size_t i=0; i<NUM_SERVOS; i++) { move_servo(servos[i], 45); }
-  delay(2000);
-}
+void loop() { }
 
 void move_servo(uint servo_pin, uint degree) {
   uint16_t cc = map(degree, SERVO_DEG_MIN, SERVO_DEG_MAX, CC_LOW, CC_HIGH);
@@ -69,5 +72,21 @@ void move_servo(uint servo_pin, uint degree) {
 }
 
 void on_can_received(int packet_size) {
-  // TODO parse & execute valve command
+  uint16_t id = can.packetId();
+  PACKET_TYPE packet_type = (PACKET_TYPE)can.read();
+  switch (packet_type) {
+    case VALVE_COMMAND_PACKET: {
+      uint16_t id;
+      can.readBytes((uint8_t*)&id, 2);
+      bool position = can.read();
+
+      digitalWrite(LED_BUILTIN, position);
+
+      if (id == SERVO_1_ID) { 
+        move_servo(PIN_SERVO_1, position ? DEG_OPEN : DEG_CLOSED);
+      } else if (id == SERVO_2_ID) {
+        move_servo(PIN_SERVO_2, position ? DEG_OPEN : DEG_CLOSED);        
+      }
+    } break;
+  }
 }
